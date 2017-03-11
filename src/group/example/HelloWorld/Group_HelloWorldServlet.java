bba4d501e5f5b1eb49088b316ba25979126be3d6
minhquan2904 <minhquan2904@outlook.com>
2017-03-11 10:37:53 +0700

    deploy web to gg-cloud

---
+++ src/group/example/HelloWorld/Group_HelloWorldServlet.java
@@ -0,0 +1,12 @@
+package group.example.HelloWorld;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Group_HelloWorldServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
